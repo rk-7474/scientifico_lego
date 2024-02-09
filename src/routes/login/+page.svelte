ceb4ee62749dfdaf8155821226c5d0ddbf224ba9
tommlazzza <tommaso.lazzarin@icloud.com>
2024-02-09 20:20:22 +0100

    login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,11 +2,20 @@
 	import { enhance } from "$app/forms";
 </script>
 
-<h1>Sign in</h1>
-<form method="post" use:enhance>
-	<label for="username">Username</label>
-	<input name="username" id="username" /><br />
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" /><br />
-	<button>Continue</button>
-</form>
+
+<div class="w-full h-full">
+	<div class="flex flex-col items-center h-full justify-center bg-cover bg-no-repeat bg-indigo-950" style="background-image: url(/src/routes/img/arthub.png);">
+		<div>
+			<h1 class="font-bold text-white text-6xl py-4">Sign in</h1>
+		</div>
+		<div class=" bg-[#76448A] rounded-lg shadow-2xl p-10">
+			<form class="text-center" method="post" use:enhance>
+				<label class=" text-white px-2" for="username">Username</label>
+				<input class="my-2 outline-none rounded shadow-lg p-1" name="username" id="username" /><br />
+				<label class="text-white px-2" for="password">Password</label>
+				<input class="my-2 outline-none rounded shadow-lg p-1" type="password" name="password" id="password" /><br />
+				<button class="p-2 mt-5 mb-0 rounded-lg border-solid border-2 border-white text-white hover:bg-white hover:text-black hover:drop-shadow-2xl hover:scale-110 transition">Continue</button>
+			</form>
+		</div>
+	</div>
+</div>
